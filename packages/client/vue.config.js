d48bf885a4e5a73d2026725e657551bb1549a971
Sam Farrow <sfarrow@bioiq.com>
2020-03-09 13:14:48 -0400

    - Update build process to build brotli / gzip

--- packages/client/vue.config.js
+++ packages/client/vue.config.js
@@ -1,5 +1,7 @@
 
 const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
+const BrotliPlugin = require('brotli-webpack-plugin')
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
@@ -11,5 +13,20 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js'
     }
     config.entry.app = './src/main.js'
+    config.plugins = [...config.plugins,
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.vue$/,
+        threshold: 10240,
+        minRatio: 0.7
+      }),
+      new BrotliPlugin({
+        filename: '[path].br[query]',
+        test: /\.js$|\.css$|\.html$|\.vue$/,
+        threshold: 10240,
+        minRatio: 0.7
+      })
+    ]
   }
 }
